a043f068581c720e1e065004f0e9bb4c10d7dec5
jhclaura <linkinmonkey@gmail.com>
2018-10-14 23:18:14 -0400

    Unity - socket connect; still trying to convert base64image string

--- idfa/unity/marrow/Assets/Socket/SocketCommunication.cs
+++ idfa/unity/marrow/Assets/Socket/SocketCommunication.cs
@@ -6,75 +6,117 @@ using BestHTTP.SocketIO;
 using Newtonsoft.Json;
 
 [Serializable]
-public class Text2ImageRequestInfo
+public class AttnGanRequestData
 {
 	public string caption;
 }
 
 public class SocketCommunication : MonoBehaviour {
 
-	public string t2iURL;
-	private SocketManager t2iSocketManager;
-	private Socket t2iSocket;
+	public string attnGanUrl; //http://api.marrow.raycaster.studio:3333/socket.io/
+	public int attnGanImageWidth = 256;
+	public int attnGanImageHeight = 256;
+	public float typingTimerLength = 0.5f;
+	public Material attnGanMaterial;
+	public Texture2D testTexture;
+
+	private SocketManager attnGanSocketManager;
+	private Socket attnGanSocket;
+
+	public Texture2D attnGanTexture;
+	private float lastTypingTimecode;
 
 	private void Start ()
 	{
-
 		SocketOptions options = new SocketOptions();
 		options.AutoConnect = false;
 		//options.ConnectWith = BestHTTP.SocketIO.Transports.TransportTypes.WebSocket;
 
-		t2iSocketManager = new SocketManager(new Uri(t2iURL), options);
-		//t2iSocket = t2iSocketManager.Socket;
-		t2iSocket = t2iSocketManager["/query"];
-
-		t2iSocket.On("connecting",
-		                 (socket, packet, args) => Debug.Log("connecting")
-		                );
-		t2iSocket.On("event",
-                         (socket, packet, args) => Debug.Log("event")
-                        );
-		t2iSocket.On("connect", OnT2IConnect);
-		t2iSocket.On("update_response", OnT2IUpdateResponse);
-		t2iSocket.On(SocketIOEventTypes.Error, OnError);
+		attnGanSocketManager = new SocketManager(new Uri(attnGanUrl), options);
+		attnGanSocketManager.Encoder = new BestHTTP.SocketIO.JsonEncoders.LitJsonEncoder();
         
-		t2iSocketManager.Open();
+		//attnGanSocket = attnGanSocketManager.Socket;
+		//attnGanSocket = attnGanSocketManager.GetSocket("/query");
+		attnGanSocket = attnGanSocketManager["/query"];
+        
+		attnGanSocket.On("connecting", (socket, packet, args) => Debug.Log("Connecting AttnGan"));
+		attnGanSocket.On(SocketIOEventTypes.Connect, OnAttnGanConnect);
+		attnGanSocket.On("update_response", OnAttnGanUpdateResponse);
+		attnGanSocket.On(SocketIOEventTypes.Error, OnAttnGanError);
+        
+		attnGanSocketManager.Open();
+
+        // Image convert related
+		//byte[] testBase64Img = testTexture.GetRawTextureData();
+		//Debug.Log("test image format: " + testTexture.format);
+		attnGanTexture = new Texture2D(attnGanImageWidth, attnGanImageHeight, TextureFormat.RGB24, false, false);
+		//attnGanTexture.LoadRawTextureData(testBase64Img);
+		//attnGanTexture.Apply();
+
+		attnGanMaterial.mainTexture = attnGanTexture;
+		//byte[] base64Img = attnGanTexture.GetRawTextureData();
 	}
 
 	private void Update()
 	{
 		if (Input.GetKeyDown("a"))
 		{
-			Text2ImageRequestInfo text2ImageRequestInfo = new Text2ImageRequestInfo();
-			text2ImageRequestInfo.caption = "apple test";
-			//string json = JsonConvert.SerializeObject(text2ImageRequestInfo);
-			string json = JsonUtility.ToJson(text2ImageRequestInfo);
-			t2iSocket.Emit("update_request", json);
-			//t2iSocket.Emit("update_request", "caption", "apple");
-			Debug.Log("emit update_request");
+			AttnGanRequestData attnGanRequestData = new AttnGanRequestData();
+			attnGanRequestData.caption = "apple test";
+
+            // v.1 - send as dictionary
+            /*
+            Dictionary<string, object> arg = new Dictionary<string, object>();
+			arg.Add("caption", "apple");
+			t2iSocket.Emit("update_request", arg);
+            */
+
+            // v.2 - send as object
+			//string json = JsonConvert.SerializeObject(attnGanRequestData);
+			//string json = JsonUtility.ToJson(attnGanRequestData);
+			attnGanSocket.Emit("update_request", attnGanRequestData);
+			Debug.Log("AttnGanSocket emit update_request");
 		}
 	}
 
 	private void OnDestroy()
 	{
-		Debug.Log("Close pix2pix socket");
-		t2iSocketManager.Close();
+		Debug.Log("Close AttnGAN socket");
+		attnGanSocketManager.Close();
 	}
 
-	void OnT2IConnect(Socket socket, Packet packet, params object[] args)
+	void OnAttnGanConnect(Socket socket, Packet packet, params object[] args)
 	{
-		Debug.Log("Connect to t2i socket server.");
+		Debug.Log("Connected to AttnGAN.");
+		Debug.Log(socket.Id);
 	}
 
-	void OnT2IUpdateResponse(Socket socket, Packet packet, params object[] args)
+	void OnAttnGanUpdateResponse(Socket socket, Packet packet, params object[] args)
 	{
-		//Dictionary<string, object> data = args[0] as Dictionary<string, object>;
-		//string base64Image = data["image"] as string;
-		Debug.Log("Get t2i: update_response");
+		Dictionary<string, object> data = args[0] as Dictionary<string, object>;
+		string base64Image = data["image"] as string;
+		Debug.Log("Got AttnGAN update_response - base64Image: " + base64Image);
+
+		byte[] receivedBase64Img = Convert.FromBase64String(base64Image);
+		//Debug.Log("received base64Img length: " + receivedBase64Img.Length);
+		attnGanTexture.LoadRawTextureData(receivedBase64Img);
+		attnGanTexture.Apply();
 	}
 
-	void OnError(Socket socket, Packet packet, params object[] args)
+	void OnAttnGanError(Socket socket, Packet packet, params object[] args)
 	{
 		Debug.LogError(string.Format("--- Error --- {0}", args[0].ToString()));
+	}
+
+    public void OnAttnGanInputUpdate(string inputText)
+	{
+		if ((Time.time - lastTypingTimecode) <= typingTimerLength)
+			return;
+		
+		AttnGanRequestData attnGanRequestData = new AttnGanRequestData();
+		attnGanRequestData.caption = inputText;
+        attnGanSocket.Emit("update_request", attnGanRequestData);
+
+		lastTypingTimecode = Time.time;
 	}
 }
