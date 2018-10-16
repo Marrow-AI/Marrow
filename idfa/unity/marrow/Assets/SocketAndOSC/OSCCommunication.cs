8c59830cbe8c8ee73544976e50710dfe03e407df
jhclaura <linkinmonkey@gmail.com>
2018-10-16 00:28:29 -0400

    Unity - osc to socket

---
+++ idfa/unity/marrow/Assets/SocketAndOSC/OSCCommunication.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC;
+
+namespace Marrow
+{
+	public class OSCCommunication : MonoBehaviour
+	{
+		public string address = "/speech";
+		[Header("OSC Settings")]
+		public OSCReceiver oscReceiver;
+
+		void Start()
+		{
+			oscReceiver.Bind(address, ReceivedMessage);
+		}
+
+		void ReceivedMessage (OSCMessage message)
+		{
+			Debug.LogFormat("Received: {0}", message);
+		}
+	}
+}
