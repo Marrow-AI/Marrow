e021b7bdf825d68e88178e064b61a1aea4926575
Avnerus <avnerus@gmail.com>
2018-11-07 03:59:28 +0200

    new responses

--- idfa/audio/generate_others.py
+++ idfa/audio/generate_others.py
@@ -5,26 +5,26 @@ moz_tts = MozillaTTS()
 script = Script()
 
 #intro
-#try:
-#    moz_tts.say(script.data["intro"], "gan_intro/intro.wav")
-#except Exception as e:
-#    print("Error generating intro {}".format(e))
+try:
+    moz_tts.say(script.data["intro"], "gan_intro/intro.wav")
+except Exception as e:
+    print("Error generating intro {}".format(e))
 
 
 #question
-#try:
-#    moz_tts.say(script.data["question"]["line"], "gan_question/line.wav")
-#except Exception as e:
-#    print("Error generating question line {}".format(e))
-#index = 0
-#for timeout in script.data["question"]["timeouts"]:
-#    try:
-#        moz_tts.say(timeout["line"], "gan_question/timeout{}.wav".format(index))
-#    except Exception as e:
-#        print("Error generating question {}".format(e))
-#    finally:
-#        index += 1
-#
+try:
+    moz_tts.say(script.data["question"]["line"], "gan_question/line.wav")
+except Exception as e:
+    print("Error generating question line {}".format(e))
+index = 0
+for timeout in script.data["question"]["timeouts"]:
+    try:
+        moz_tts.say(timeout["line"], "gan_question/timeout{}.wav".format(index))
+    except Exception as e:
+        print("Error generating question {}".format(e))
+    finally:
+        index += 1
+
 try:
     moz_tts.say(script.data["pre-script"], "gan_intro/pre_script.wav")
 except Exception as e:
