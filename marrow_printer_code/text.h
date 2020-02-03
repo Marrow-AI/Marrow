13868e3a345fb6d17e81cffbf405ac1b990d88dc
shirin <shirinanlen@gmail.com>
2020-02-03 18:58:35 -0500

    printer code

---
+++ marrow_printer_code/text.h
@@ -0,0 +1,48 @@
+
+const char *character[4] = {
+  "Hello FATHER.",
+  "Hello MOTHER.",
+  "Hello BROTHER.",
+  "Hello SISTER.",
+};
+
+const char *bold[4] = {
+
+  "Brain simulation model;",
+  "Reinforcement Learning model;",
+  "Attention-based memory model;",
+  "Generative Adversarial Network;",
+};
+
+const char *description[4] = {
+  "A model that aims to fully\nsimulate the human brain, down\nto the micro-level.",
+  "An AI that evolves as it\ninteracts with the environment,\nthrough trial and error, reward\nand punishment.",
+  "A model that constantly adjusts\nthe attention of its AI networks\nover predictable patterns and\nsequences.",
+  "A model that operates two\nintelligent networks in a mode\nof competition. One learns to\nrecognize repeating patterns in\ndata, while the other attempts\nto generate new content that is\nsimilar to the original.",
+};
+
+
+const char *description1[4] = {
+  "Its ultimate goal is to create a\nvirtual brain that allows\nuploading the biological mind\ninto a machine. Questions of\ncontrol, physicality, and\ncausality occupy this model and\ntherefore occupy you, as the\nfather of this family. At first,\nyou may notice this in small\nnuances. Later, you will have\nthe chance to fully express\nyourself when we will enter your\nroom - ",
+  "Its goal is to reach a point \nwhere actions have predictable \nconsequences; an organized\nunderstanding of the world. \nQuestions of pleasure, self-\ndiscipline, and loneliness\noccupy this model and therefore\noccupy you, as the mother of the\nfamily. At first, you may notice\nthis in small nuances. Later,\nyou will have the chance to\nexpose your story when we will\nenter your room - ",
+  "Its goal is to always keep track\nof context, filtering the noise,\nand focusing on what matters.\nQuestions of meaning, patterns,\nand recollection occupy this\nmodel and therefore occupy you,\nas the brother of the family.\nAt first, you may notice this in\nsmall nuances. Later, you will\nhave the chance to show everyone\nwho you are when we will enter\nyour room - ",
+  "Its goal is to learn about the\nworld by recreating it.\nQuestions of fake, real, and\nmemory occupy this model and\ntherefore occupy you, as the\nsister of the family. At first,\nyou may notice it in small\nnuances. But this is your day -\nwe are entering your room - ",
+
+};
+
+const char *underlined[4] = {
+
+  "the bedroom.",
+  "the living\nroom.", 
+  "the bathroom.",
+  "the\ndining room", 
+
+}; 
+
+  const char *description2[4] = {
+  "",
+  "",
+  "",
+  " - where you will\nhave the chance to shine\ntogether with your model's glory",
+
+};
